cdd1dc54474e0eca677f23b0e665e7a1b4276893
wisdomis <108566232+wisdomis@users.noreply.github.com>
2024-08-02 20:30:46 +0900

    feat. 짝꿍맺기, 해제 시 문구 나오도록 설정

--- lib/pages/lover_make_delete.dart
+++ lib/pages/lover_make_delete.dart
@@ -26,14 +26,29 @@ class _ProfilePageState extends State<ProfilePage> {
   void _toggleFriendStatus() {
     setState(() {
       _isFriend = !_isFriend;
-      final message = _isFriend
-          ? '$_partnerName 님과 짝꿍이 되었습니다!'
-          : '$_partnerName 님과 짝꿍이 해제되었습니다';
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(message),
-        ),
-      );
+      if (_isFriend) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('$_partnerName 님과 짝꿍이 되었습니다!'),
+          ),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Row(
+              children: [
+                Expanded(child: Text('짝꿍과 끊어질 경우 해당 계약서는 없어지며 코드가 변경됩니다. 정말로 $_partnerName 님과 짝꿍을 끊으시겠습니까?')),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(); // 돌아가기 버튼을 눌렀을 때 첫 화면으로 돌아가기
+                  },
+                  child: Text('돌아가기'),
+                ),
+              ],
+            ),
+          ),
+        );
+      }
     });
   }
 
@@ -55,83 +70,112 @@ class _ProfilePageState extends State<ProfilePage> {
           },
         ),
       ),
-      body: ListView(
-        shrinkWrap: true,
-        physics: const BouncingScrollPhysics(),
-        children: [
-          // Section 1 - Profile Picture Wrapper
-          Container(
-            color: Colors.blue,
-            padding: const EdgeInsets.symmetric(vertical: 24),
-            child: GestureDetector(
-              onTap: () {
-                print('짝꿍 프로필 사진 클릭');
-              },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 130,
-                    height: 130,
-                    margin: const EdgeInsets.only(bottom: 15),
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(100),
-                      // 상대 프로필 사진
-                      image: const DecorationImage(image: AssetImage('assets/images/profile.png'), fit: BoxFit.cover),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        '짝꿍 프로필 사진',
-                        style: TextStyle(fontFamily: 'inter', fontWeight: FontWeight.w600, color: Colors.white),
-                      ),
-                      const SizedBox(width: 8),
-                      SvgPicture.asset('assets/icons/camera.svg', color: Colors.white),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
-          // Section 2 - 상대 정보 Wrapper
-          Container(
-            margin: const EdgeInsets.only(top: 24),
-            width: MediaQuery.of(context).size.width,
+      body: _isFriend ? _buildFriendInfo() : _buildNoFriendInfo(context),
+    );
+  }
+
+  Widget _buildFriendInfo() {
+    return ListView(
+      shrinkWrap: true,
+      physics: const BouncingScrollPhysics(),
+      children: [
+        // Section 1 - Profile Picture Wrapper
+        Container(
+          color: Colors.blue,
+          padding: const EdgeInsets.symmetric(vertical: 24),
+          child: GestureDetector(
+            onTap: () {
+              print('짝꿍 프로필 사진 클릭');
+            },
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const UserInfoTile(
-                  margin: EdgeInsets.only(bottom: 16),
-                  label: '이메일',
-                  value: 'otheruser@example.com',
-                ),
-                UserInfoTile(
-                  margin: const EdgeInsets.only(bottom: 16),
-                  label: '이름',
-                  value: _partnerName,
-                ),
-
                 Container(
-                  margin: const EdgeInsets.all(16),
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: _toggleFriendStatus,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: _isFriend ? Colors.red : Colors.blue,  // 버튼 색상 변경
-                    ),
-                    child: Text(
-                      _isFriend ? '짝꿍 해제' : '짝꿍 맺기',
-                      style: const TextStyle(color: Colors.white),
-                    ),
+                  width: 130,
+                  height: 130,
+                  margin: const EdgeInsets.only(bottom: 15),
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(100),
+                    // 상대 프로필 사진
+                    image: const DecorationImage(image: AssetImage('assets/images/profile.png'), fit: BoxFit.cover),
                   ),
                 ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      '짝꿍 프로필 사진',
+                      style: TextStyle(fontFamily: 'inter', fontWeight: FontWeight.w600, color: Colors.white),
+                    ),
+                    const SizedBox(width: 8),
+                    SvgPicture.asset('assets/icons/camera.svg', color: Colors.white),
+                  ],
+                )
               ],
             ),
+          ),
+        ),
+        // Section 2 - 상대 정보 Wrapper
+        Container(
+          margin: const EdgeInsets.only(top: 24),
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const UserInfoTile(
+                margin: EdgeInsets.only(bottom: 16),
+                label: '이메일',
+                value: 'otheruser@example.com',
+              ),
+              UserInfoTile(
+                margin: const EdgeInsets.only(bottom: 16),
+                label: '이름',
+                value: _partnerName,
+              ),
+
+              Container(
+                margin: const EdgeInsets.all(16),
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: _toggleFriendStatus,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: _isFriend ? Colors.red : Colors.blue,  // 버튼 색상 변경
+                  ),
+                  child: Text(
+                    _isFriend ? '짝꿍 해제' : '짝꿍 맺기',
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildNoFriendInfo(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            '연결된 짝꿍이 없어요!\n짝꿍을 연결 해볼까요?',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ConnectCouplePage()),
+              );
+            },
+            child: const Text('짝꿍 연결하기'),
           ),
         ],
       ),
@@ -173,6 +217,21 @@ class UserInfoTile extends StatelessWidget {
             Text(value, style: const TextStyle(fontFamily: 'inter', fontWeight: FontWeight.w400, fontSize: 14)),
           ],
         ),
+      ),
+    );
+  }
+}
+
+// connect_couple_page.dart
+class ConnectCouplePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Connect Couple Page'),
+      ),
+      body: const Center(
+        child: Text('This is the Connect Couple Page.'),
       ),
     );
   }
